2499fcfa8d959b8f087f04864b90f47db7d4d690
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-09-16 16:05:57 +0000

    feat: added blog update endpoint

--- src/modules/blog/blog.controller.ts
+++ src/modules/blog/blog.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Post, Body, HttpStatus, Request } from '@nestjs/common';
+import { Controller, Post, Body, HttpStatus, Request, Patch, Param } from '@nestjs/common';
 import { BlogService } from './blog.service';
 import { CreateBlogDTO } from './dto/create-blog.dto';
 import { BlogResponseDTO } from './dto/blog-response-dto';
-import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiOkResponse, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UpdateBlogDTO } from './dto/update-blog.dto';
 
 @ApiTags('Blogs')
-@Controller('blog')
+@Controller('blogs')
 export class BlogController {
   constructor(private readonly blogService: BlogService) {}
 
@@ -18,5 +19,17 @@ export class BlogController {
   @ApiInternalServerErrorResponse({description: 'Internal server error'})
   create(@Body() createBlogDto: CreateBlogDTO, @Request() req): Promise<BlogResponseDTO> {
     return this.blogService.createBlog(createBlogDto, req.user);
+  }
+
+  @Patch(':id')
+  @ApiBearerAuth()
+  @ApiOperation({summary: "Update Blog"})
+  @ApiParam({ name: 'id', type: 'string' })
+  @ApiBody({type: UpdateBlogDTO})
+  @ApiOkResponse({ description: 'Blog updated successfully!', type: BlogResponseDTO })
+  @ApiBadRequestResponse({description: 'Bad Request.' })
+  @ApiInternalServerErrorResponse({description: 'Internal server error'})
+  update(@Param('id') id: string, @Body() updateBlogDto: UpdateBlogDTO, @Request() req): Promise<BlogResponseDTO> {
+    return this.blogService.updateBlog(updateBlogDto, req.user, id);
   }
 }
--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,13 +1,15 @@
-import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CreateBlogDTO } from './dto/create-blog.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Blog, BlogStatus } from './entities/blog.entity';
+import { Blog } from './entities/blog.entity';
 import { Repository } from 'typeorm';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { BlogResponseDTO } from './dto/blog-response-dto';
 import { UserService } from '../user/user.service';
-import { CustomInternalServerErrorException, CustomNotFoundException } from '../../helpers/custom-exceptions';
+import { CustomForbiddenException, CustomInternalServerErrorException, CustomNotFoundException } from '../../helpers/custom-exceptions';
 import { Logger } from 'winston';
+import { UpdateBlogDTO } from './dto/update-blog.dto';
+import { UserDTO } from '../user/dto/user.dto';
 
 @Injectable()
 export class BlogService {
@@ -17,7 +19,7 @@ export class BlogService {
     private userService: UserService,
   ) { }
 
-  async createBlog(createBlogDto: CreateBlogDTO, currentUser: any): Promise<BlogResponseDTO> {
+  async createBlog(createBlogDto: CreateBlogDTO, currentUser: UserDTO): Promise<BlogResponseDTO> {
     try {
       const user = await this.userService.getUserById(currentUser.sub);
       if (!user) {
@@ -51,6 +53,57 @@ export class BlogService {
     } catch (error) {
       this.logger.error(`Error while creating blog: ${error.stack}`);
       throw new CustomInternalServerErrorException('Error while creating blog');
+    }
+  }
+
+  async updateBlog(updateBlogDto: UpdateBlogDTO, currentUser: UserDTO, blogId: string): Promise<BlogResponseDTO> {
+    try {
+      const user = await this.userService.getUserById(currentUser.sub);
+      if (!user) {
+        throw new CustomNotFoundException('User');
+      }
+
+      const blog = await this.blogRepository.findOne({
+        where: {id: blogId},
+        relations: ['user', 'categories'],
+      });
+
+      if (!blog) {
+        throw new CustomNotFoundException('Blog');
+      }
+
+      if (blog.user.id !== user.id){
+        throw new CustomForbiddenException('You are not authorized to update this blog')
+      }
+
+      Object.assign(blog, updateBlogDto);
+      
+      const savedBlog = await this.blogRepository.save(blog);
+
+      const blogResponse: BlogResponseDTO = {
+        statusCode: 200,
+        message: 'Blog updated successfully',
+        data: {
+          id: savedBlog.id,
+          title: savedBlog.title,
+          content: savedBlog.content,
+          imageUrls: savedBlog.imageUrls,
+          tags: savedBlog.tags,
+          status: savedBlog.status,
+          scheduledAt: savedBlog.scheduledAt,
+          likes: savedBlog.likes,
+          dislikes: savedBlog.dislikes,
+          isSeeded: savedBlog.isSeeded,
+          author: `${user.firstName} ${user.lastName}`,
+          createdAt: savedBlog.createdAt,
+          updatedAt: savedBlog.updatedAt,
+        }
+      }
+
+      return blogResponse;
+    } catch (error) {
+      this.logger.error(`Error while updating blog: ${error.stack}`);
+      throw new CustomInternalServerErrorException('Error while updating blog');
     }
   }
 }
